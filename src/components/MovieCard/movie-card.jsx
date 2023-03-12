c072f0592d65e545b8b082a2775a2b32346fc3b7
Brendon Choi <choi.brendon2001@gmail.com>
2023-03-13 04:40:54 +0900

    Update movie-card.jsx

---
+++ src/components/MovieCard/movie-card.jsx
@@ -0,0 +1,23 @@
+export const MovieCard = ({ movie }) => {
+    return ( 
+      <Card className="movieCard mt-3 mb-3">
+      <Card.Img variant="top" src={movie.image} />
+      <Card.Body>
+        <Card.Title>{movie.title}</Card.Title>
+        <Card.Text className="text-truncate">{movie.director.Name}</Card.Text>
+        <Link to={`/movies/${encodeURIComponent(movie.id)}`}>
+          <Button variant="link" className="movie-button">Open</Button>
+        </Link>
+      </Card.Body>
+    </Card>
+  );
+};
+   
+//here is where we define all the props constraints for the MovieCard
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    Title: PropTypes.string,
+    image: PropTypes.string,
+  //  director: PropTypes.string
+  }).isRequired,
+}; 
