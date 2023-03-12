795e7451961d63b27b9357e5d621d16824b176a9
Brendon Choi <choi.brendon2001@gmail.com>
2023-03-13 02:22:32 +0900

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,8 @@
+import { createRoot } from "react-dom/client";
 
+import { MainView } from "./components/main-view/main-view";
 
-import { createRoot } from 'react-dom/client';
-
-// Import statement to indicate that you need to bundle `./index.scss`
 import "./index.scss";
-
 // Main component (will eventually use all the others)
 const MyFlixApplication = () => {
   return (
