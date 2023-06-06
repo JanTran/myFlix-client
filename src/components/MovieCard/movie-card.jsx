0bffb7d0d200c003c574a36d6e2d0d0135176f40
Brendon Choi <choi.brendon2001@gmail.com>
2023-06-07 07:21:22 +0900

    Update movie-card.jsx

--- src/components/MovieCard/movie-card.jsx
+++ src/components/MovieCard/movie-card.jsx
@@ -1,22 +1,65 @@
-import React from "react";
 import PropTypes from "prop-types";
-import { Button, Card } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
+import {Row, Col, Card} from "react-bootstrap";
+
+import { FavoriteButton } from "../favorite-button/favorite-button";
 
 import "./movie-card.scss";
 
-export const MovieCard = ({ movie, onClick }) => {
+export const MovieCard= ({ movieData, user, token, favoriteMovies }) => {
   return (
-    <div className="movieCard mt-3 mb-3">
-      <div onClick={() => onClick(movie)}>{movie.Title}</div>
-    </div>
-  );
+      <>
+      <Row>
+      <Col></Col>
+      </Row>
+      <Card
+        className="text-bg-dark border-secondary rounded mt-3"
+      >
+          <Col className="text-end">
+            <FavoriteButton 
+              user={user} 
+              token={token} 
+              movieData={movieData}
+              favoriteMovies={favoriteMovies}
+            />
+          </Col>
+          <Link to={`/movies/${movieData._id}`} className="movie-card">
+          <Card.Img className="px-3 pt-3" src={movieData.ImageURL}/>
+          <Card.Body >
+            <Card.Title className="fw-bold cardTitle">
+              {movieData.Title} ({movieData.Release_date})              
+            </Card.Title>
+            <Card.Text 
+              className="fw-semibold border-top border-bottom border-secondary"
+            >
+              Genre: <br className="mobile"/>{movieData.Genre.Name}
+            </Card.Text>
+            <Card.Text 
+              className="fw-semibold border-top border-bottom border-secondary"
+            >
+              Director: <br className="mobile"/>{movieData.Director.Name}
+            </Card.Text>
+            <Card.Text 
+              className="border-top border-bottom border-secondary"
+            >
+              {movieData.Description}
+            </Card.Text>           
+          </Card.Body>
+          </Link>
+      </Card>
+      </>
+  )
 };
 
-//here is where we define all the props constraints for the MovieCard
-MovieCard.propTypes = {
-  movie: PropTypes.shape({
-    Title: PropTypes.string,
-    ImagePath: PropTypes.string,
-  }).isRequired,
-  onClick: PropTypes.func,
-};
+MovieCard.propTypes= {
+  movieData: PropTypes.shape({
+    Title: PropTypes.string.isRequired,
+    Description: PropTypes.string.isRequired,
+    Release_date: PropTypes.string.isRequired,
+    Genre: PropTypes.object.isRequired,
+    Director: PropTypes.object.isRequired,
+    Actors: PropTypes.array,
+    ImageURL: PropTypes.string.isRequired
+  })
+}
