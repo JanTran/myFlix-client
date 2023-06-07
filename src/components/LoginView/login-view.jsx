8ba5d7d2d0d8d5e3d3f428f12ba3705e05558349
Brendon Choi <choi.brendon2001@gmail.com>
2023-06-08 07:07:18 +0900

    Update login-view.jsx

--- src/components/LoginView/login-view.jsx
+++ src/components/LoginView/login-view.jsx
@@ -13,7 +13,7 @@ export const LoginView = ({ onLoggedIn }) => {
       Password: password
     };
 
-    fetch('https://myflix-brendon.herokuapp.com/movies', {
+    fetch('https://myflix-brendon.herokuapp.com/login', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
