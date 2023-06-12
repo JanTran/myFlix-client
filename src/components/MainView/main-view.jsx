56f0d02eab748c0d7ac53af1e52f08914bb14ece
Brendon Choi <choi.brendon2001@gmail.com>
2023-06-13 00:54:43 +0900

    Update main-view.jsx

--- src/components/MainView/main-view.jsx
+++ src/components/MainView/main-view.jsx
@@ -29,6 +29,29 @@ export const MainView = () => {
       ))}
     </div>
   );
+  const data = {
+      Username: username,
+      Password: password
+    };
+  fetch("https://myflix-brendon.herokuapp.com/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "App/json"
+      },
+      body: JSON.stringify(data)
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Login response: ", data);
+        if (data.user) {
+          onLoggedIn(data.user, data.token);
+        } else {
+          alert("No such user");
+        }
+      })
+      .catch((e) => {
+        alert("Something went wrong");
+      });
 };
 
 /*
