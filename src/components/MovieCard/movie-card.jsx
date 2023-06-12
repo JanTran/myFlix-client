70163503a84c8e4139ca0d90d5e18f5ca072daee
schnecke <eiweala@live.com>
2023-06-12 10:33:03 -0700

    save

--- src/components/MovieCard/movie-card.jsx
+++ src/components/MovieCard/movie-card.jsx
@@ -1,65 +1,44 @@
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 
-import {Row, Col, Card} from "react-bootstrap";
-
-import { FavoriteButton } from "../favorite-button/favorite-button";
+import { Card, Col } from "react-bootstrap";
 
 import "./movie-card.scss";
 
-export const MovieCard= ({ movieData, user, token, favoriteMovies }) => {
+export const MovieCard = ({ movie }) => {
   return (
-      <>
-      <Row>
-      <Col></Col>
-      </Row>
-      <Card
-        className="text-bg-dark border-secondary rounded mt-3"
-      >
-          <Col className="text-end">
-            <FavoriteButton 
-              user={user} 
-              token={token} 
-              movieData={movieData}
-              favoriteMovies={favoriteMovies}
-            />
-          </Col>
-          <Link to={`/movies/${movieData._id}`} className="movie-card">
-          <Card.Img className="px-3 pt-3" src={movieData.ImageURL}/>
-          <Card.Body >
-            <Card.Title className="fw-bold cardTitle">
-              {movieData.Title} ({movieData.Release_date})              
-            </Card.Title>
-            <Card.Text 
-              className="fw-semibold border-top border-bottom border-secondary"
-            >
-              Genre: <br className="mobile"/>{movieData.Genre.Name}
-            </Card.Text>
-            <Card.Text 
-              className="fw-semibold border-top border-bottom border-secondary"
-            >
-              Director: <br className="mobile"/>{movieData.Director.Name}
-            </Card.Text>
-            <Card.Text 
-              className="border-top border-bottom border-secondary"
-            >
-              {movieData.Description}
-            </Card.Text>           
-          </Card.Body>
-          </Link>
+    <Col md={3}>
+      <Card className="text-bg-dark border-secondary rounded mt-3">
+        <Card.Img className="px-3 pt-3" src={movie.ImagePath} />
+        <Card.Body>
+          <Card.Title className="fw-bold cardTitle border-top pt-2">
+            {movie.Title}
+          </Card.Title>
+          <Card.Text className="fw-semibold border-top border-secondary">
+            Genre: <br className="mobile" />
+            {movie.Genre.Name}
+          </Card.Text>
+          <Card.Text className="fw-semibold border-top border-secondary">
+            Director: <br className="mobile" />
+            {movie.Director.name}
+          </Card.Text>
+          <Card.Text className="border-top border-bottom border-secondary">
+            {movie.Description}
+          </Card.Text>
+        </Card.Body>
       </Card>
-      </>
-  )
+    </Col>
+  );
 };
 
-MovieCard.propTypes= {
-  movieData: PropTypes.shape({
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
     Title: PropTypes.string.isRequired,
     Description: PropTypes.string.isRequired,
     Release_date: PropTypes.string.isRequired,
     Genre: PropTypes.object.isRequired,
     Director: PropTypes.object.isRequired,
     Actors: PropTypes.array,
-    ImageURL: PropTypes.string.isRequired
-  })
-}
+    ImagePath: PropTypes.string.isRequired,
+  }),
+};
--- src/components/MovieView/movie-view.jsx
+++ src/components/MovieView/movie-view.jsx
@@ -1,30 +1,23 @@
 import { useParams } from "react-router";
 import { Link } from "react-router-dom";
 
-import {Row, Col, Button} from "react-bootstrap"
+import { Row, Col, Button } from "react-bootstrap";
 
-import { FavoriteButton } from "../favorite-button/favorite-button";
-import { MovieCard } from "../movie-card/movie-card";
+import { MovieCard } from "../MovieCard/movie-card";
 
-export const MovieView= ({ user, token, movieData, favoriteMovies }) => {
-const { movieId } = useParams();
+export const MovieView = ({ user, token, movieData, favoriteMovies }) => {
+  const { movieId } = useParams();
 
-const Movie = movieData.find((movie) => movie._id === movieId);
-const similarMovies= movieData.filter((m) => Movie.Genre.Name === m.Genre.Name);
+  const Movie = movieData.find((movie) => movie._id === movieId);
+  const similarMovies = movieData.filter(
+    (m) => Movie.Genre.Name === m.Genre.Name
+  );
 
-
-  return(
+  return (
     <>
       <Row className="mt-3">
-        <Col>
-          <FavoriteButton 
-            user={user} 
-            token={token} 
-            movieData={Movie}
-            favoriteMovies={favoriteMovies}
-          />
-        </Col>
-        <Col sm={{offset: 10}}>
+        <Col></Col>
+        <Col sm={{ offset: 10 }}>
           <Link to={`/`}>
             <Button>Back</Button>
           </Link>
@@ -34,49 +27,61 @@ const similarMovies= movieData.filter((m) => Movie.Genre.Name === m.Genre.Name);
         <Col>{Movie.Title}</Col>
       </Row>
       <Row className="border-top border-bottom border-secondary">
-        <Col xs= {5} sm={2}>Genre: </Col>
+        <Col xs={5} sm={2}>
+          Genre:{" "}
+        </Col>
         <Col xs={10}>{Movie.Genre.Name}</Col>
       </Row>
       <Row className="border-top border-bottom border-secondary">
-        <Col xs= {5} sm={2}>Description: </Col>
+        <Col xs={5} sm={2}>
+          Description:{" "}
+        </Col>
         <Col xs={10}>{Movie.Description}</Col>
       </Row>
       <Row className="border-top border-bottom border-secondary">
-        <Col xs= {5} sm={2}>Director: </Col>
-        <Col xs={10} className="fs-6">{Movie.Director.Name}</Col>
+        <Col xs={5} sm={2}>
+          Director:{" "}
+        </Col>
+        <Col xs={10} className="fs-6">
+          {Movie.Director.Name}
+        </Col>
       </Row>
       <Row className="border-top border-bottom border-secondary">
-        <Col xs= {5} sm={2}>Release date: </Col>
+        <Col xs={5} sm={2}>
+          Release date:{" "}
+        </Col>
         <Col xs={10}>{Movie.Release_date}</Col>
       </Row>
       <Row className="border-top border-bottom border-secondary">
-        <Col xs= {5} sm={2}>Actors: </Col>
+        <Col xs={5} sm={2}>
+          Actors:{" "}
+        </Col>
         <Col xs={10}>{Movie.Actors}</Col>
       </Row>
       <Row>
-        <Col className="d-flex justify-content-center mt-4"><img src={Movie.ImageURL} className="movie_image"/></Col>
-      </Row>
-      <Row>
-        <Col className="fs-2">
-          Similar Movies:
+        <Col className="d-flex justify-content-center mt-4">
+          <img src={Movie.ImageURL} className="movie_image" />
         </Col>
       </Row>
       <Row>
-        { similarMovies.map((similarMovie) => (
+        <Col className="fs-2">Similar Movies:</Col>
+      </Row>
+      <Row>
+        {similarMovies.map((similarMovie) =>
           similarMovie._id === Movie._id ? (
             <Col className="d-none" key="none"></Col>
           ) : (
-          <Col xs={12} md={6} lg={4} xl={3} xxl={2} key={similarMovie._id}>
-            <MovieCard
-              movieData={similarMovie}
-              user={user}
-              token={token}
-              favoriteMovies={favoriteMovies}
-            />
-          </Col>
+            <Col xs={12} md={6} lg={4} xl={3} xxl={2} key={similarMovie._id}>
+              <MovieCard
+                movieData={similarMovie}
+                user={user}
+                token={token}
+                favoriteMovies={favoriteMovies}
+              />
+            </Col>
           )
-          ))}
+        )}
       </Row>
     </>
-  )
-}
+  );
+};
